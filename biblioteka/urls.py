db7464aa8455e2c79ea4108b6e48af054f1924ed
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-06-29 10:43:18 -0300

    feat: changing the project name

--- biblioteka/urls.py
+++ biblioteka/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for projetoFinal project.
+URL configuration for biblioteka project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- biblioteka/wsgi.py
+++ biblioteka/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projetoFinal project.
+WSGI config for biblioteka project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetoFinal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteka.settings')
 
 application = get_wsgi_application()
