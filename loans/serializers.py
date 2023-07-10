5bfeb27fb7cc18f2eb40241ad5dd3c2ae78100c5
1marcosaquino <1marcosaquino@gmail.com>
2023-07-10 16:09:50 -0300

    feat:add create and update loan

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,35 +1,17 @@
 from rest_framework import serializers
 from .models import Loan
 from users.models import User
+from libraries.models import Library, UserLibraryBlock
 from copies.models import Copy
-from books.models import Book
-import calendar
-import datetime
-from datetime import date
-from dateutil.relativedelta import relativedelta
+from books.models import Book, Following
 from django.shortcuts import get_object_or_404
-from django.db.models import F
-from copies.serializers import CopySerializer
+from django.db.models import F, Q
 from users.serializers import UserSerializer
 
-
-def devolution_date():
-    end_date = date.today() + relativedelta(days=+15)
-    name_day = calendar.day_name[datetime.datetime.strptime(
-        str(end_date), '%Y-%m-%d').weekday()]
-
-    if name_day == 'Saturday':
-        end_date = date.today() + relativedelta(days=+17)
-
-    if name_day == 'Sunday':
-        end_date = date.today() + relativedelta(days=+16)
-    return end_date
-
+from .utils import Dates
 
 
 class LoanSerializer(serializers.ModelSerializer):
-    # copy = CopySerializer(read_only=True)
-    # user_id = UserSerializer(read_only=True)
 
     class Meta:
         model = Loan
@@ -38,40 +20,56 @@ class LoanSerializer(serializers.ModelSerializer):
             "start_date",
             "end_date",
             "devolution_date",
-            "user_id",
+            "library",
+            "user",
             "copy"
         ]
+        depth = 1
         extra_kwargs = {
             "start_date": {"read_only": True},
             "copy": {"read_only": True},
         }
-            
 
     def create(self, validated_data: dict) -> Loan:
 
-        user_id = self.context['request'].data
+        request = self.context['request'].data
         copy_id = self.context['view'].kwargs['pk']
 
-        get_object_or_404(User, pk=user_id["user_id"])
+        user = get_object_or_404(User, pk=request["user_id"])
+        library = get_object_or_404(Library, pk=request["library_id"])
         copy = get_object_or_404(Copy, pk=copy_id)
 
-        if copy.availability is False:
+        loan_list = Loan.objects.all().filter(user_id=request["user_id"])
+        today = Dates().today
+
+        for loan in loan_list:
+            if today > loan.end_date and loan.devolution_date == None:
+                UserLibraryBlock.objects.create(
+                    user=user, library=library).save()
+
+                raise serializers.ValidationError(
+                    {"Error": "Devolve logo o que vc pegou! :-P "})
+
+        if copy.is_available is False:
             raise serializers.ValidationError(
                 {"Error": "Currently unavailable"})
 
         book = Book.objects.get(pk=copy.book_id)
-        book_availability = book.quantity
+        follow = Following.objects.all().filter(book=book)
+        book_quantity = book.quantity
 
-        if book_availability < 1:
-            Copy.objects.filter(pk=copy.book_id).update(
-                quantity=F('quantity') - 1)
+        if book_quantity < 1:
+            Copy.objects.filter(book=book).update(
+                is_available=Q(is_available=False))
+
             raise serializers.ValidationError(
                 {"Error": "Copy not currently available"})
 
         payload = {
-            "end_date": str(devolution_date()),
-            "user_id": user_id["user_id"],
+            "end_date": str(Dates().devolution_date(10)) if len(follow) >= 10 < 20 else str(Dates().devolution_date(7)) if len(follow) >= 20 else str(Dates().devolution_date(15)),
+            "user_id": request["user_id"],
             "copy_id": copy_id,
+            "library_id": request["library_id"]
         }
 
         Book.objects.filter(pk=copy.book_id).update(quantity=F('quantity') - 1)
