404837762bbba58d75a3912511aa2a0b643925d4
1marcosaquino <1marcosaquino@gmail.com>
2023-07-11 15:23:28 -0300

    done

--- loans/serializers.py
+++ loans/serializers.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 from .models import Loan
 from users.models import User
-from libraries.models import Library, UserLibraryBlock
+from libraries.models import Library
 from copies.models import Copy
 from books.models import Book, Following
 from django.shortcuts import get_object_or_404
 from django.db.models import F, Q
 from users.serializers import UserSerializer
-from .utils import Dates
+from .utils import Dates, user_is_blocked, block_user, send_email, unblocked_user
 from libraries.serializers import LibrarySerializer
 from copies.serializers import CopySerializer
 
@@ -35,27 +35,35 @@ class LoanSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data: dict) -> Loan:
+
         request = self.context["request"].data
         copy_id = self.context["view"].kwargs["pk"]
         user = get_object_or_404(User, pk=request["user_id"])
         library = get_object_or_404(Library, pk=request["library_id"])
         copy = get_object_or_404(Copy, pk=copy_id)
-        loan_list = Loan.objects.all().filter(user_id=request["user_id"])
-        today = Dates().today
-        for loan in loan_list:
-            if today > loan.end_date and loan.devolution_date == None:
-                UserLibraryBlock.objects.create(user=user, library=library).save()
-                raise serializers.ValidationError(
-                    {"Error": "Devolve logo o que vc pegou! :-P "}
-                )
-        if copy.is_available is False:
-            raise serializers.ValidationError({"Error": "Currently unavailable"})
+
         book = Book.objects.get(pk=copy.book_id)
         follow = Following.objects.all().filter(book=book)
-        book_quantity = book.quantity
-        if book_quantity < 1:
-            Copy.objects.filter(book=book).update(is_available=Q(is_available=False))
-            raise serializers.ValidationError({"Error": "Copy not currently available"})
+        loan_list = Loan.objects.all().filter(user_id=request["user_id"])
+
+        today = Dates().today
+        for loan in loan_list:
+            if user_is_blocked(user, library):
+                if Dates().unblocked_date(loan.devolution_date):
+                    unblocked_user(user, library)
+                raise serializers.ValidationError(
+                    {"Error": "User Blocked haha"}
+                )
+
+            if today > loan.end_date and loan.devolution_date == None:
+                block_user(user, library)
+                raise serializers.ValidationError(
+                    {"Error": "User Blocked HOHO"})
+
+        if copy.is_available is False or book.quantity < 1:
+            raise serializers.ValidationError(
+                {"Error": "Copy not Currently unavailable"})
+
         payload = {
             "end_date": str(Dates().devolution_date(10))
             if len(follow) >= 10 < 20
@@ -66,18 +74,37 @@ class LoanSerializer(serializers.ModelSerializer):
             "copy_id": copy_id,
             "library_id": request["library_id"],
         }
+
         Book.objects.filter(pk=copy.book_id).update(quantity=F("quantity") - 1)
-        return Loan.objects.create(**payload)
+        created = Loan.objects.create(**payload)
+
+        book = Book.objects.get(pk=copy.book_id)
+
+        if book.quantity < 1:
+            send_email(user, f"The book {book.title} currently available")
+            Copy.objects.filter(book=book).update(
+                is_available=Q(is_available=False))
+
+        return created
 
     def update(self, instance: Loan, validated_data: dict) -> Loan:
         if instance.devolution_date is not None:
             raise serializers.ValidationError(
                 {"Error": "Copy has already been returned"}
             )
-        copy = Copy.objects.get(pk=instance.copy_id)
-        Book.objects.filter(pk=copy.book_id).update(quantity=F("quantity") + 1)
-        for key, value in validated_data.items():
+
+        for key, value in {"devolution_date": str(Dates().today)}.items():
             setattr(instance, key, value)
+
+        book = Book.objects.filter(pk=instance.copy.book_id)
+        book.update(quantity=F("quantity") + 1)
+
+        if book.first().quantity == 1:
+            send_email(instance.user,
+                       f"The book, {book.first().title} is available")
+            Copy.objects.filter(book=book.first()).update(
+                is_available=Q(is_available=True))
+
         instance.save()
         return instance
 
--- loans/views.py
+++ loans/views.py
@@ -3,18 +3,26 @@ from rest_framework import generics
 
 from .models import Loan
 from .serializers import LoanSerializer
+from rest_framework.pagination import PageNumberPagination
+from users.permissions import IsAdminOrEmployee
 
 
 class LoanCreateView(generics.CreateAPIView):
+    # permission_classes = [IsAdminOrEmployee]
+
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
 
 class LoanListView(generics.ListAPIView):
+    pagination_class = PageNumberPagination
+
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
 
 
 class LoanRetrieveView(generics.RetrieveUpdateDestroyAPIView):
+    # permission_classes = [IsAdminOrEmployee]
+
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
