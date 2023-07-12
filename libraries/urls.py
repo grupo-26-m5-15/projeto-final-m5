d9cdaa5c146679e6967928e98c6a0c19a129f770
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-07-12 11:20:04 -0300

    main: fixed library url

--- libraries/urls.py
+++ libraries/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path("libraries/<int:pk>/books/", ListLibraryBooks.as_view()),
     path("libraries/<int:pk>/loans/", ListLibraryLoans.as_view()),
     path("libraries/<int:pk>/employees/", ListLibraryEmployees.as_view()),
-    path("libraries/<int:pk>/users/block", ListLibraryUsersBlocked.as_view()),
+    path("libraries/<int:pk>/users/block/", ListLibraryUsersBlocked.as_view()),
 ]
